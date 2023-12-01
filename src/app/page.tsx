b703a3a92dc82a72bab1b72a8e97d72c171b830f
Ana Clara Kyotoku <ackyotoku@gmail.com>
2023-12-01 12:47:29 -0300

    chore: updates the initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main>teste</main>;
+  return (
+    <main>
+      <h1>Multi-language challenge</h1>
+      <p>
+        Esse será um site multi-idiomas para um desafio da Onesight. Terá três
+        páginas:
+      </p>
+      <ul>
+        <li>Home</li>
+        <li>Sobre nós</li>
+        <li>Receitas -{`>`} uma receita</li>
+      </ul>
+    </main>
+  );
 }
