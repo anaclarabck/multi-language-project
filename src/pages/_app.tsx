89cbcc10b406a7fbf342cebed50db39f0ace88cd
Ana Clara Kyotoku <ackyotoku@gmail.com>
2023-12-02 21:57:48 -0300

    config: add next intl config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+// import RootLayout from "@/app/layout";
+import { NextIntlClientProvider } from "next-intl";
+import { AppProps } from "next/app";
+import { NextRouter, withRouter } from "next/router";
+
+type Props = AppProps & {
+  router: NextRouter;
+};
+
+function App({ Component, pageProps, router }: Props) {
+  return (
+    <NextIntlClientProvider
+      locale={(router.query?.locale as string) ?? "en"}
+      messages={pageProps.messages}
+    >
+      {/* <RootLayout> */}
+      <Component {...pageProps} />
+      {/* </RootLayout> */}
+    </NextIntlClientProvider>
+  );
+}
+
+export default withRouter(App);
